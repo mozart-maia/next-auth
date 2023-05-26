8d6ff554d71b54fe6a5bf740269d57f47c3a3fd4
Mozart Maia <mozartmaia89@gmail.com>
2023-05-26 12:16:44 -0300

    fix: firestore adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,66 +5,87 @@ import GithubProvider from "next-auth/providers/github";
 import TwitterProvider from "next-auth/providers/twitter";
 import Auth0Provider from "next-auth/providers/auth0";
 import EmailProvider from "next-auth/providers/email";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-import { firestore } from "./lib/firestore";
+// import db, { firestore } from "./lib/firestore";
+import firebase, { initializeApp } from "firebase/app";
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email"
+const app = initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+});
+
+const fs: any = getFirestore(app);
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
-  adapter: FirestoreAdapter(firestore),
+  adapter: FirestoreAdapter(fs),
   providers: [
-    /* 
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "my-project",
+      credentials: {
+        email: {
+          label: "email",
+          type: "email",
+          placeholder: "jsmith@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const payload = {
+          email: credentials?.email,
+          password: credentials?.password,
+        };
 
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
+        console.log("payload", payload);
+
+        const res = await fetch(
+          "https://cloudcoders.azurewebsites.net/api/tokens",
+          {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const user = await res.json();
+        console.log(user);
+        if (!res.ok) {
+          throw new Error(user.message);
+        }
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user;
+        }
+
+        // Return null if user data could not be retrieved
+        return null;
       },
     }),
-    */
-
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: process.env.EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASSWORD,
-    //     },
-    //   },
-    //   from: process.env.EMAIL_FROM,
-    // }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_ID,
-    //   clientSecret: process.env.FACEBOOK_SECRET,
-    // }),
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID,
-    //   clientSecret: process.env.GOOGLE_SECRET,
-    // }),
-    // TwitterProvider({
-    //   clientId: process.env.TWITTER_ID,
-    //   clientSecret: process.env.TWITTER_SECRET,
-    // }),
-    // Auth0Provider({
-    //   clientId: process.env.AUTH0_ID,
-    //   clientSecret: process.env.AUTH0_SECRET,
-    //   issuer: process.env.AUTH0_ISSUER,
-    // }),
-
     {
       id: "oidcCLIENT",
       name: "Login DNA",
@@ -86,12 +107,22 @@ export const authOptions: NextAuthOptions = {
       },
     },
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   theme: {
     colorScheme: "light",
   },
   callbacks: {
-    async jwt({ token }) {
+    async jwt({ token, user, account }: any) {
       token.userRole = "admin";
+
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: user.token,
+          refreshToken: user.refreshToken,
+        };
+      }
+
       return token;
     },
   },
--- pages/api/auth/lib/firestore.js
+++ pages/api/auth/lib/firestore.js
@@ -1,17 +1,22 @@
 import { initFirestore } from "@next-auth/firebase-adapter";
 import { cert } from "firebase-admin/app";
+import { getFirestore } from "firebase/firestore";
 // import { initializeApp } from "firebase/app";
 // import { getAuth } from "firebase/auth";
 
 export const firestore = initFirestore();
 
-// export const app = initializeApp({
-//   apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: NEXT_PUBLIC_FIREBASE_APP_ID,
-// });
+const app = initializeApp({
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
+});
+
+const db = getFirestore(app);
+
+export default db;
 
 // export const auth = getAuth(firestore);
